a26299e68fc16eaf1e6356bd6f8a28b18a4be29f
satanikssbg <terzibgss@gmail.com>
2023-11-22 22:33:11 +0200

    Add react hooks in eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,11 +10,16 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-hooks'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/exhaustive-deps": ["warn", {
+      "additionalHooks": "(useMyCustomHook|useMyOtherCustomHook)"
+    }]
   },
 }
